8d798070b31e247302403310522e56f447a5fa7c
zhaocaipeng <1988881129z@163.com>
2023-05-15 10:37:38 +0800

    code init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # leoZkmlPy
 Python invoke the zk algorithm
+
+## Use Demo
+from facialRecogn.facialRecogn import my_algorithm
+
+result = my_algorithm(0)
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='leoZkmlPy',
+    version='0.1',
+    packages=['facialRecogn'],
+    install_requires=[ 
+    ],
+)
+
